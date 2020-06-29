2549fef72407530ea2a536b03a08740bcb412b3c
unknown <rizky.darmarazak@gmail.com>
2020-06-29 14:06:55 +0700

    Updates

---
+++ precache-manifest.71935338981b8d0eaa00dba4382d6ade.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "208e31ed9ba41e17aa7dd3781124cbd4",
+    "url": "/react-find-recipes/index.html"
+  },
+  {
+    "revision": "27233a4693aa657b6f57",
+    "url": "/react-find-recipes/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a90354f540a7884ae0e7",
+    "url": "/react-find-recipes/static/css/main.1385e74f.chunk.css"
+  },
+  {
+    "revision": "27233a4693aa657b6f57",
+    "url": "/react-find-recipes/static/js/2.dba325c3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-find-recipes/static/js/2.dba325c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a90354f540a7884ae0e7",
+    "url": "/react-find-recipes/static/js/main.0a18d304.chunk.js"
+  },
+  {
+    "revision": "fbd755617d0cfd0f51c8",
+    "url": "/react-find-recipes/static/js/runtime-main.939ccbdd.js"
+  }
+]);
